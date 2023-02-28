5d32835fc1f66c1cb7d457ca507b9596e9d6e3b0
Mek <nik.podterob@gmail.com>
2023-02-28 20:49:22 +0300

    Initial Commit

---
+++ stable_sort/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  stable_sort
+//
+//  Created by Никита Подтероб on 28.02.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
